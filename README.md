6487fda3e3bd830cb4ceb177179049dd861d86c1
Rebecca Turner <me@re-becca.org>
2016-09-06 00:57:03 -0700

    initial

---
+++ README.md
@@ -0,0 +1,101 @@
+Streampub
+---------
+
+A streaming EPUB3 writer.
+
+## EXAMPLE
+
+```
+var Streampub = require('streampub') 
+var epub = new Streampub({title: 'My Example'})
+epub.setAuthor('Example User')
+epub.pipe(fs.createWriteStream('example.epub'))
+epub.write({index: 0, chapterName: 'Chapter 1', fileName: 'chapter-1.xhtml', content: '<b>doc content</b>'})
+epub.end()
+```
+
+## USAGE
+
+### var epub = new Streampub(*opts*)
+
+*opts* is an object that optionally has the following properties:
+
+* *title* - The title of the epub, defaults to "Untitled"
+* *author* - The name of the author of the epub
+* *modified* - When the epub was last modified, defaults to now. (Date object)
+* *published* - When the source material was published. (Date object)
+* *source* - The original URL or URN of the source material
+* *language* - The 2 digit language code of the material, defaults to "en".
+* *description* - A brief description or summary of the material
+
+All of the options can be set after object creation with obvious setters:
+
+### epub.setTitle(title)
+### epub.setAuthor(author)
+### epub.setModified(modified)
+### epub.setPublished(published)
+### epub.setSource(source)
+### epub.setLanguage(language)
+### epub.setDescription(description)
+
+### The Streampub Object
+
+The Streampub object is a transform stream that takes chapter information as
+input and outputs binary chunks of an epub file. It's an ordinary stream so you
+can pipe into it or write to it and call `.end()` when you're done.
+
+### var epub.write(*obj*, *callback*)
+
+This is the usual stream write function. The object must have the following keys:
+
+* **chapterName** - The name of the chapter in the index.
+* **content** - The HTML content of this chapter.  This will be passed
+  through `htmltidy` in order to make it valid XHTML.
+* **index** - *(optional)* Where the chapter should show up in the index. These numbers
+  can have gaps and are used for ordering ONLY. Duplicate index values will
+  result in the earlier chapter being excluded from the index. If not specified will
+  be added after any where it _was_ specified, in the order written.
+* **fileName** - *(optional)* The filename to use *inside* the epub. This only matters if
+  you want to do links between chapters. This should end in `.xhtml`.
+
+`htmltidy` options used are:
+
+```
+    'output-xhtml': true,
+    'doctype': 'html5',
+    'add-xml-decl': true,
+    'coerce-endtags': true,
+    'enclose-block-text': true,
+    'drop-proprietary-attributes': true,
+    'strict-tags-attributes': true,
+    'clean': true,
+    'quote-nbsp': false,
+    'numeric-entities': true
+```
+
+## VALIDATION
+
+This takes care to generate only valid XML using programmatic generators and
+not templates
+
+Epubs produced by this have been validated with
+[epubcheck](https://github.com/idpf/epubcheck).  No warnings outside of
+content warnings should be present.
+
+Content warnings ordinarily only happen if your content contains broken links–usually relative links to resources
+that don't exist in the epub.
+
+## TODO
+
+It would be nice to support adding non-indexed files to the epub, specifically images.
+
+## PRIOR ART
+
+There are a bunch of epub generators already available.  Many are pre EPUB3. 
+Most work off of files on disk rather than in memory constructs.  Only one
+other provides a stream that I was able to find was
+[epub-generator](https://npmjs.com/package/epub-generator) and it only
+provides a read stream.  I wanted to be able to build a full pipeline so
+that, for instance, the whole pipeline would support backpressure.  I also
+very much wanted to be able to set epub metadata after object construction
+time.
---
+++ index.js
@@ -0,0 +1,202 @@
+'use strict'
+var Bluebird = require('bluebird')
+var Transform = require('readable-stream').Transform
+var zlib = require('zlib')
+var ZipStream = require('zip-stream')
+var inherits = require('util').inherits
+var tidy = Bluebird.promisify(require('htmltidy').tidy)
+var xml = require('xml')
+var uuid = require('uuid')
+
+// there's a bug in htmltidy where the first invocation sometimes results in an empty result =/
+function retidy (content, opts, count) {
+  if (!count) count = 1
+  return tidy(content, opts).then(function (html) {
+    if (count < 20 && content.length !== 0 && html.length === 0) return retidy(content, opts, ++count)
+    return html
+  })
+}
+
+module.exports = Streampub
+module.exports.newChapter = Chapter
+
+var container = {container: [
+  {_attr: {version: '1.0', xmlns: 'urn:oasis:names:tc:opendocument:xmlns:container'}},
+  {rootfiles: [
+    {rootfile: [
+      {_attr: {'full-path': 'OEBPS/content.opf', 'media-type': 'application/oebps-package+xml'}}
+    ]}
+  ]}
+]}
+
+function Streampub (opts) {
+  var self = this
+  Transform.call(this, {objectMode: true})
+  if (!opts) opts = {}
+  this.zip = new ZipStream({level: zlib.Z_BEST_COMPRESSION})
+  this.zip.entry = Bluebird.promisify(this.zip.entry)
+  this.zip.on('data', function (data, enc) {
+    self.push(data, enc)
+  })
+  this.chapters = []
+  this.files = []
+  this.meta = {}
+  this.meta.title = opts.title || 'Untitled'
+  this.meta.author = opts.author
+  this.setModified(opts.modified || new Date())
+  if (opts.published) this.setPublished(opts.published)
+  this.meta.source = opts.source
+  this.meta.language = opts.language || 'en'
+  this.meta.description = opts.description
+  this.maxId = 0
+  this.header = self.zip.entry('application/epub+zip', {name: 'mimetype'}).then(function () {
+    return self.zip.entry(xml(container, {declaration: true}), {name: 'META-INF/container.xml'})
+  })
+}
+inherits(Streampub, Transform)
+
+Streampub.prototype._flush = function (done) {
+  var self = this
+  var pkg = []
+  pkg.push({_attr: {version: '3.0', 'unique-identifier': 'pub-id', 'xmlns': 'http://www.idpf.org/2007/opf'}})
+  pkg.push({metadata: self._generateMetadata()})
+  pkg.push({manifest: self._generateManifest()})
+  pkg.push({spine: self._generateSpine()})
+  self.header.then(function () {
+    return self.zip.entry(xml([{'package': pkg}], {declaration: true}), {name: 'OEBPS/content.opf'})
+  }).then(function () {
+    return self.zip.entry(xml([{html: self._generateTOC()}], {declaration: true}), {name: 'OEBPS/toc.xhtml'})
+  }).then(function () {
+    self.zip.once('finish', done)
+    self.zip.finalize()
+  })
+}
+
+function Chapter (index, chapterName, fileName, content) {
+  return {index: index, chapterName: chapterName, fileName: fileName, content: content}
+}
+
+Streampub.prototype._transform = function (data, encoding, done) {
+  var self = this
+  var tidyOpt = {
+    'output-xhtml': true,
+    'doctype': 'html5',
+    'add-xml-decl': true,
+    'coerce-endtags': true,
+    'enclose-block-text': true,
+    'drop-proprietary-attributes': true,
+    'strict-tags-attributes': true,
+    'clean': true,
+    'quote-nbsp': false,
+    'numeric-entities': true
+  }
+  retidy(data.content, tidyOpt).catch(done).then(function (html) {
+
+    var id = ++self.maxId
+    var index = data.index || (100000 + id)
+    var fileName = data.fileName || ('streampub-chapter-' + id + '.xhtml')
+    self.chapters[index] = {index: index, chapterName: data.chapterName, fileName: fileName}
+    self.files.push({fileName: fileName, mime: 'application/xhtml+xml', id: 'file' + id})
+    self.header.then(function () {
+      return self.zip.entry(html, {name: 'OEBPS/' + fileName})
+    }).then(function () { done() }, done)
+  })
+}
+
+Streampub.prototype.setTitle = function (title) {
+  this.meta.title = title
+}
+
+Streampub.prototype.setAuthor = function (author) {
+  this.meta.author = author
+}
+
+Streampub.prototype.setModified = function (modified) {
+  if (!(modified instanceof Date)) modified = new Date(modified)
+  this.meta.modified = modified
+}
+
+Streampub.prototype.setPublished = function (published) {
+  if (!(published instanceof Date)) published = new Date(published)
+  this.meta.published = published
+}
+
+Streampub.prototype.setSource = function (src) {
+  this.meta.source = src
+}
+
+Streampub.prototype.setLanguage = function (language) {
+  this.meta.language = language
+}
+
+Streampub.prototype.setDescription = function (description) {
+  this.meta.description = description
+}
+
+Streampub.prototype.finalize = function (cb) {
+}
+
+function w3cdtc (date) {
+  try {
+    return date.toISOString().replace(/[.]\d{1,3}Z/, 'Z')
+  } catch (e) {
+    console.error('WAT', date, '!!')
+    throw e
+  }
+}
+
+Streampub.prototype._generateMetadata = function () {
+  var metadata = [{_attr: {'xmlns:dc': 'http://purl.org/dc/elements/1.1/'}}]
+  metadata.push({'dc:identifier': [{_attr: {id: 'pub-id'}}, 'url:uuid:' + uuid.v4()]})
+  metadata.push({'dc:language': this.meta.language})
+  metadata.push({'dc:title': this.meta.title})
+  metadata.push({'meta': [{_attr: {property: 'dcterms:modified'}}, w3cdtc(this.meta.modified)]})
+  if (this.meta.source) {
+    metadata.push({'dc:source': this.meta.source})
+  }
+  if (this.meta.author) {
+    metadata.push({'dc:creator': [{_attr: {id: 'author'}}, this.meta.author]})
+    metadata.push({'meta': [{_attr: {refines: '#author', property: 'role', scheme: 'marc:relators', id: 'role'}}, 'aut']})
+  }
+  if (this.meta.description) {
+    metadata.push({'dc:description': this.meta.description})
+  }
+  if (this.meta.published) {
+    metadata.push({'dc:date': w3cdtc(this.meta.published)})
+  }
+  return metadata
+}
+
+Streampub.prototype._generateManifest = function () {
+  var manifest = []
+  // epub2: <item href="toc.ncx" id="ncx" media-type="application/x-dtbncx+xml" />
+  // epub3: <item href="toc.xhtml" id="nav" properties="nav" media-type: "application/xhtml+xml" />
+  manifest.push({'item': [{_attr: {id: 'nav', href: 'toc.xhtml', properties: 'nav', 'media-type': 'application/xhtml+xml'}}]})
+  this.files.forEach(function (file) {
+    manifest.push({'item': [{_attr: {id: file.id, href: file.fileName, 'media-type': file.mime}}]})
+  })
+  return manifest
+}
+
+Streampub.prototype._generateSpine = function () {
+  var spine = []
+  this.files.forEach(function (file) {
+    spine.push({'itemref': [{_attr: {idref: file.id}}]})
+  })
+  return spine
+}
+
+Streampub.prototype._generateTOC = function () {
+  var html = [{_attr: {'xmlns': 'http://www.w3.org/1999/xhtml', 'xmlns:epub': 'http://www.idpf.org/2007/ops'}}]
+  html.push({'head': []})
+  var body = []
+  html.push({'body': body})
+  var nav = [{_attr: {'epub:type': 'toc'}}]
+  body.push({'nav': nav})
+  var ol = []
+  nav.push({'ol': ol})
+  this.chapters.forEach(function (chapter) {
+    ol.push({'li': [{'a': [{_attr: {'href': chapter.fileName}}, chapter.chapterName]}]})
+  })
+  return html
+}
